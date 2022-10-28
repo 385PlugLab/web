d39624dd9661f3ec49d44a5596b7cd90a8c6dd6a
kjbpftw <100333017+kjbpftw@users.noreply.github.com>
2022-10-27 19:11:01 -0600

    Update AccountNav.js

--- src/components/AccountNav/AccountNav.js
+++ src/components/AccountNav/AccountNav.js
@@ -11,33 +11,62 @@ const AccountNav = (props) => {
   return (
     <div className={styles.root}>
       <div className={styles.webRoot}>
+         <Link
+          activeClassName={styles.activeLink}
+          to={'/account/new-order/'}
+          className={styles.webLink}
+        >
+          New Order
+        </Link>
+ <Link
+          activeClassName={styles.activeLink}
+          to={'/account/qr/'}
+          className={styles.webLink}
+        >
+          Display QR Code
+        </Link>
         <Link
           activeClassName={styles.activeLink}
           to={'/account/orders/'}
           className={styles.webLink}
         >
-          Orders
+          History
         </Link>
         <Link
           activeClassName={styles.activeLink}
           to={'/account/address/'}
           className={styles.webLink}
         >
-          Addresses
+          Meeting Locations
         </Link>
-        <Link
-          activeClassName={styles.activeLink}
-          to={'/account/settings/'}
-          className={styles.webLink}
-        >
-          Settings
-        </Link>
+       
         <Link
           activeClassName={styles.activeLink}
           to={'/account/viewed/'}
           className={styles.webLink}
         >
-          Recently Viewed
+          Favorites
+        </Link>
+ <Link
+          activeClassName={styles.activeLink}
+          to={'/account/connections/'}
+          className={styles.webLink}
+        >
+          My People
+        </Link>
+ <Link
+          activeClassName={styles.activeLink}
+          to={'/account/wallet/'}
+          className={styles.webLink}
+        >
+          Wallet
+        </Link>
+ <Link
+          activeClassName={styles.activeLink}
+          to={'/account/settings/'}
+          className={styles.webLink}
+        >
+          Settings
         </Link>
         <span
           role={'presentation'}
